de6abb5982ab823ee1254c67f814d52a227f5a1d
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-25 14:23:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(callback) {
+  callback(value, index, array) {
+    array[index] = "I changed " + array[index] + "!!!";
+  }
+}
